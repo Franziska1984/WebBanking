4c2b7d9cc1f289ae00a5c9bb39d987c0f14988cd
Franziska1984 <franziska.wienecke@gmx.de>
2022-02-28 11:02:15 +0100

    Entity angelegt

--- src/main/java/de/telekom/sea7/model/implementation/ZahlungImpl.java
+++ src/main/java/de/telekom/sea7/model/implementation/ZahlungImpl.java
@@ -1,12 +1,24 @@
 package de.telekom.sea7.model.implementation;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import org.springframework.stereotype.Service;
 
 import de.telekom.sea7.interfaces.Zahlung;
 
+@Entity  //gibt an, dass das Objekt von Spring Data verwaltet wird 
+         //und dass seine Attribute in Spalten von DB-Tabellen konvertiert werden
 @Service
 public class ZahlungImpl implements Zahlung{
 
+	@Id   //gibt an, dass das Attribut als Primärschlüssel in der Tabelle
+	      //verwendet werden soll, die der Entität entspricht.
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	 //was angibt, dass der Wert der ID beim Einfügen 
+	 //in die Datenbank automatisch und inkrementell generiert wird.
 	private String empfaenger;
 	private String iban;
 	private String bic;
